45f695a65957cd9bf0fd8f88bb04abc47a20db45
Hans K <hans@communitycomputing.net>
2025-09-26 11:09:49 -0400

    Use actual date pickers; provide defaults

--- calendar_print/forms.py
+++ calendar_print/forms.py
@@ -1,6 +1,8 @@
-from django.forms import Form
+from django.forms import Form, DateInput
+from django.utils import timezone
+from datetime import timedelta
 from django.forms.fields import DateField
 
 class CalendarPrintForm(Form):
-    start_date = DateField()
-    end_date = DateField()
+    start_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date())
+    end_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date() + timedelta(days=7))
