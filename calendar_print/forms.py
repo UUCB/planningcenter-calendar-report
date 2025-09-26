41218d80808172cac83dd0ee74a4ffdba98d58aa
Hans K <hans@communitycomputing.net>
2025-09-26 12:02:34 -0400

    Initial sort of working report

--- calendar_print/forms.py
+++ calendar_print/forms.py
@@ -1,10 +1,11 @@
 from django.forms import Form, DateInput
 from django.utils import timezone
 from datetime import timedelta
-from django.forms.fields import DateField, BooleanField
+from django.forms.fields import DateField, BooleanField, CharField
 
 
 class CalendarPrintForm(Form):
     start_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date())
     end_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date() + timedelta(days=7))
+    header_text = CharField(required=False, initial="Events This Week")
     refresh = BooleanField(initial=True, required=False)
--- calendar_print/views.py
+++ calendar_print/views.py
@@ -5,6 +5,21 @@ from calendar_print.forms import CalendarPrintForm
 from calendar_print.models import Event
 from planningcenter_calendar_report import planningcenter_extras
 
+from datetime import datetime, date, timedelta
+
+def daterange(start_date: date, end_date: date):
+    days = int((end_date - start_date).days)
+    for n in range(days):
+        yield start_date + timedelta(n)
+
+
+class DayWithEvents():
+    def __init__(self, my_date):
+        self.date = my_date
+
+    def events(self):
+        return Event.objects.filter(date=self.date)
+
 class CalendarPrintView(FormView):
     template_name = 'calendar_print/options.html'
     form_class = CalendarPrintForm
@@ -16,7 +31,8 @@ class CalendarPrintView(FormView):
             self.request,
             'calendar_print/report.html',
             {
-                'events': Event.objects.filter(start_time__gte=form.cleaned_data['start_date'], start_time__lte=form.cleaned_data['end_date']),
+                'header_text': form.cleaned_data['header_text'],
+                'days': [DayWithEvents(day) for day in daterange(form.cleaned_data['start_date'], form.cleaned_data['end_date'])],
             }
         )
 
