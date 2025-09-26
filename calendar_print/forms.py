80d8e897a924a3c2334ce49a4bba51b447f28b2b
Hans Kelson <hans@communitycomputing.net>
2025-09-26 16:02:11 -0400

    Default start_date and end_date to the next two sundays, respectively

--- calendar_print/forms.py
+++ calendar_print/forms.py
@@ -3,9 +3,15 @@ from django.utils import timezone
 from datetime import timedelta
 from django.forms.fields import DateField, BooleanField, CharField
 
+def get_sunday():
+    sunday = timezone.now().date() + timedelta(days=6 - timezone.now().weekday() % 7)
+    return sunday
+
+def get_following_sunday():
+    return get_sunday() + timedelta(days=7)
 
 class CalendarPrintForm(Form):
-    start_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date())
-    end_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=timezone.now().date() + timedelta(days=7))
+    start_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=get_sunday)
+    end_date = DateField(widget=DateInput(attrs={'type': 'date'}), initial=get_following_sunday)
     header_text = CharField(required=False, initial="Events This Week")
     refresh = BooleanField(initial=True, required=False)
